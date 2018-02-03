b55a233e970344863f06ca1f2211c53d4040c333
Елена <@DESKTOP-50F8DLG>
2018-02-04 00:06:16 +0300

    Базовый класс

---
+++ addressbook-web-test/addressbook-web-test/GroupCreation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTest : TestBase
+    {
+        [Test]
+        public void GroupCreation()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitNewGroupCreation();
+            FillGroupForm(new GroupData("name", "header", "footer"));
+            SubmitGroupCreation();
+            ReturnGroupPage();
+            Logout();
+        }               
+    }
+}
---
+++ addressbook-web-test/addressbook-web-test/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactRemovalTest : TestBase
+    {     
+
+        [Test]
+        public void ContactRemoval()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin", "secret"));
+            SelectContact(1);
+            RemoveContact();
+            Logout();
+        }                             
+    }
+}
